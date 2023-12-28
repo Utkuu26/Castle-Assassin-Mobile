2e90b18cf9af310b3d7d1342752692c4daba2dc4
Utku Gezensoy <utkugezensoy@gmail.com>
2023-12-29 01:55:35 +0300

    Hiding Barrels Completed + Dungeon Pack Added

--- Castle Assassin Mobile/Assets/Scripts/Hide.cs
+++ Castle Assassin Mobile/Assets/Scripts/Hide.cs
@@ -1,18 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Hide : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform teleportTarget; // Belirli bir hedef noktası
+    private bool isHidden;
+    private bool isJoystickEnabled = true; // Joystick etkileşimini kontrol etmek için
+    public GameObject player;
+
     void Start()
     {
-        
+        isHidden = false;
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            HidePlayer();
+            StartCoroutine(DisableJoystickForDuration(1f)); // Joystick'i belirli bir süre için devre dışı bırak
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Burada sadece karakterin varilden çıktığını belirten bir işaret bırakıyoruz
+            isHidden = false;
+        }
+    }
+
     void Update()
     {
-        
+        // Update fonksiyonunda kontrolü yaparak karakterin varilden çıkmasını sağlıyoruz
+        if (isHidden && Input.GetMouseButton(0) && isJoystickEnabled)
+        {
+            ShowPlayer();
+        }
+    }
+
+    private void HidePlayer()
+    {
+        isHidden = true;
+        // Make the player invisible
+        player.SetActive(false);
+
+        // Işınlanma
+        if (teleportTarget != null)
+        {
+            player.transform.position = teleportTarget.position;
+        }
+    }
+
+    private void ShowPlayer()
+    {
+        isHidden = false;
+        // Make the player visible again
+        player.SetActive(true);
+    }
+
+    private IEnumerator DisableJoystickForDuration(float duration)
+    {
+        isJoystickEnabled = false;
+        yield return new WaitForSeconds(duration);
+        isJoystickEnabled = true;
     }
 }
