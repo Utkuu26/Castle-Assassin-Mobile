bb31c423799134f59a26bf00c0c2034c129e0362
Utku Gezensoy <utkugezensoy@gmail.com>
2024-01-03 00:59:14 +0300

    Key UI Functionallity Completed

---
+++ Castle Assassin Mobile/Assets/Scripts/KeyController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+   public GameObject uiPanel;
+    public GameObject keyImg1;
+    public GameObject keyImg2;
+
+    private bool uiPanelOpened = false;
+    private bool key1Collected = false;
+    private bool key2Collected = false;
+
+    void Start()
+    {
+        uiPanel.SetActive(false);
+        keyImg1.SetActive(false);
+        keyImg2.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (gameObject.CompareTag("Key1") && !key1Collected)
+            {
+                key1Collected = true;
+                keyImg1.SetActive(true);
+                OpenUIPanel();
+                gameObject.SetActive(false);
+            }
+            else if (gameObject.CompareTag("Key2") && !key2Collected)
+            {
+                key2Collected = true;
+                keyImg2.SetActive(true);
+                OpenUIPanel();
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OpenUIPanel()
+    {
+        if (uiPanel != null)
+        {
+            uiPanel.SetActive(true);
+        }
+    }
+}
