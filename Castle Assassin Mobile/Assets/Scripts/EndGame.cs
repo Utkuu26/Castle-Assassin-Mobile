bd6415adcd3d57eb4b60b2dcc9c791372816ed86
Utku Gezensoy <utkugezensoy@gmail.com>
2024-01-06 00:24:56 +0300

    Hint and Tutorial Arrow Animations Created

--- Castle Assassin Mobile/Assets/Scripts/EndGame.cs
+++ Castle Assassin Mobile/Assets/Scripts/EndGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGame : MonoBehaviour
 {
@@ -8,11 +9,12 @@ public class EndGame : MonoBehaviour
     public GameObject nextLevel;
     public GameObject currentLevel;
     public CharacterController characterController;
-
+    public Button continueBtn;
 
     void Start() 
     {
         nextLevel.SetActive(false);
+        continueBtn.onClick.AddListener(NextLevel);
     }
 
     void OnTriggerEnter(Collider other)
@@ -30,6 +32,7 @@ public class EndGame : MonoBehaviour
         {
             nextLevel.SetActive(true);
             currentLevel.SetActive(false);
+            endGamePanel.SetActive(false);
             characterController.enabled = true;
         }
     }
